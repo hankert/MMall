512aadebb7dc11982686b720a99fe2c3fff9d927
hanker <jinhui0106@126.com>
2017-10-10 17:47:26 +0800

    底部bottom封装1

--- latte-ec/src/main/java/com/diabin/latte/ec/main/index/IndexDelegate.java
+++ latte-ec/src/main/java/com/diabin/latte/ec/main/index/IndexDelegate.java
@@ -2,10 +2,18 @@ package com.diabin.latte.ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.diabin.latte.delegates.button.BottomItemDelegate;
 import com.diabin.latte.ec.R;
+import com.diabin.latte.ec.R2;
+import com.joanzapata.iconify.widget.IconTextView;
+
+import butterknife.BindView;
 
 
 /**
@@ -15,6 +23,17 @@ import com.diabin.latte.ec.R;
  */
 
 public class IndexDelegate extends BottomItemDelegate{
+
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView = null;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout = null;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar = null;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan = null;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView = null;
 
 
     @Override
